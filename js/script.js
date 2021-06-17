9aaa91f0789512dafdce9fd6f8e60d30905ef175
prusichka <denchuk.prusak@gmail.com>
2021-06-17 15:49:09 +0300

    commit slider2

---
+++ js/script.js
@@ -0,0 +1,12 @@
+new Swiper(".first-screen-slider", {
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+new Swiper(".our__productions__slider", {
+  loop: true,
+  slidesPerView: 4,
+});
